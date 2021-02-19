8c97133ccbac333bdde183003bf39fa29f971523
xiayulu <zuiaiqiansen@163.com>
2021-02-19 22:08:13 +0800

    update config

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from os import  environ, path
 from dotenv import load_dotenv
-from flask_caching import Cache
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
@@ -8,23 +7,20 @@ load_dotenv(path.join(basedir, '.env'))
 class Base:
     SECRET_KEY = environ.get('SECRET_KEY')
     SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
-    FLASK_APP = environ.get('FLASK_APP')
+    #FLASK_APP = 'sophia'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
     CACHE_TYPE = 'simple'
     CACHE_DEFAULT_TIMEOUT = 300
-  
-class Product(Base):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = environ.get('PRODUCT_DATABASE_URI')
     SQLALCHEMY_ECHO = False
-
+  
 class Dev(Base):
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
     SQLALCHEMY_ECHO = True
 
 class SMS:
